24abe874b652fa8c67ee6259963e0934735935e7
unknown <nathansilvacosta2303@gmail.com>
2025-09-04 16:06:31 -0300

    Criado os endpoints das funcionalidades de clientes

---
+++ src/routes/clientRoutes.js
@@ -0,0 +1,19 @@
+const express = require('require')
+
+const router = express.Router()
+
+const clientController = require('../controller/clientController.js')
+
+router.get('/', clientController.getAllClients)
+
+router.get('/:id', clientController.getClientById)
+
+router.get('/:nome', clientController.getClientByName)
+
+router.post('/', clientController.createClient)
+
+router.put('/:id', clientController.updateClient)
+
+router.delete('/:id', clientController.deleteClient)
+
+module.exports = router
