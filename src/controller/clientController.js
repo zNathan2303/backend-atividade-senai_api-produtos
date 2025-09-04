076c568b66a6791a20f75057f2c1bae6dec233eb
unknown <nathansilvacosta2303@gmail.com>
2025-09-04 15:35:22 -0300

    Criada a lógica que processará as requisições de relacionadas a clientes no clientController.js

---
+++ src/controller/clientController.js
@@ -0,0 +1,70 @@
+const clientModel = require('../model/clientModel.js')
+
+const getAllClients = (req, res) => {
+    const clientes = clientModel.findALl()
+    res.status(200).json(clientes)
+}
+
+const getClientById = (req, res) => {
+    const id = parseInt(req.params.id)
+
+    const cliente = clientModel.findById(id)
+
+    if (cliente) {
+        res.status(200).json(cliente)
+    } else {
+        res.status(404).json({ mensagem: 'Cliente não encontrado no banco de dados!' })
+    }
+}
+
+const getClientByName = (req, res) => {
+    const nome = parseInt(req.params.nome)
+
+    const cliente = clientModel.findByName(nome)
+
+    if (cliente) {
+        res.status(200).json(cliente)
+    } else {
+        res.status(404).json({ mensagem: 'Cliente não encontrado no banco de dados!' })
+    }
+}
+
+const createClient = (req, res) => {
+    const { nome, email, telefone, endereco, dataCadastro, ativo } = req.body
+
+    if (!nome || !email || !telefone || !endereco || !dataCadastro || !ativo) {
+        return res.status(400).json({ mensagem: 'Nome, email, telefone, endereço, data de cadastro, e estado de atividade são obrigatórios' })
+    } else {
+        const cliente = clientModel.createClient({ nome, email, telefone, endereco, dataCadastro, ativo })
+        res.status(201).json(cliente)
+    }
+}
+
+const updateClient = (req, res) => {
+    const id = parseInt(req.params.id)
+
+    const { nome, email, telefone, endereco, dataCadastro, ativo } = req.body
+
+    if (!nome && !email && !telefone && !endereco && !dataCadastro && !ativo) {
+        return res.status(400).json({ mensagem: 'É necessário informar algum valor que será alterado!' })
+    } else {
+        clientModel.updateClient(id, { nome, email, telefone, endereco, dataCadastro, ativo })
+        res.status(200).json({ mensagem: 'Informações alteradas!' })
+    }
+
+}
+
+const deleteClient = (req, res) => {
+    const id = parseInt(req.params.id)
+    clientModel.deleteClient(id)
+    res.status(200).json({ mensagem: 'Usuário excluído!' })
+}
+
+module.exports = {
+    getAllClients,
+    getClientById,
+    getClientByName,
+    createClient,
+    updateClient,
+    deleteClient
+}
