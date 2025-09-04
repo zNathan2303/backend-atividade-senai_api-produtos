e26620dfc299546ecfc09d0c3349b2ad6dfb1dae
unknown <nathansilvacosta2303@gmail.com>
2025-09-04 16:04:07 -0300

    Criado os endpoints das funcionalidades de produtos

---
+++ src/routes/productRoutes.js
@@ -0,0 +1,19 @@
+const express = require('require')
+
+const router = express.Router()
+
+const productController = require('../controller/productController.js')
+
+router.get('/', productController.getAllProducts)
+
+router.get('/:id', productController.getProductById)
+
+router.get('/:nome', productController.getProductByName)
+
+router.post('/', productController.createProduct)
+
+router.put('/:id', productController.updateProduct)
+
+router.delete('/:id', productController.deleteProduct)
+
+module.exports = router
